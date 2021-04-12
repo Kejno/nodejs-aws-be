c79d73fdb9c2825d547b291ec64524413a537be2
arymarchuk <Artsion_Rymarchuk@epam.com>
2021-04-12 12:46:44 +0300

    add try catch

--- src/product-service/getAllProducts/getAllProducts.js
+++ src/product-service/getAllProducts/getAllProducts.js
@@ -2,5 +2,11 @@ import { products } from '../../products'
 import { createResponse } from '../../utils/api-response';
 
 export const getAllProducts = async () => {
-    return createResponse(200, { products, count: products.length })
+    try {
+
+        return createResponse(200, { products, count: products.length });
+    } catch (error) {
+        return createResponse(500, error, true);
+
+    }
 };
